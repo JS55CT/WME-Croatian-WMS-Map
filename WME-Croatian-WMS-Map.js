683925ca1134d911abe8dba098621d10d6d0f192
Justin <145566709+JS55CT@users.noreply.github.com>
2024-12-03 10:09:24 -0500

    Create WME-Croatian-WMS-Map.js

---
+++ WME-Croatian-WMS-Map.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name			Croatian WMS layers in WME
+// @version			2024.12.03.00
+// @author			JS55CT
+// @description		Displays layers from Croatian WMS services in WME
+// @match 			https://*.waze.com/*editor*
+// ==/UserScript==
+ 
+var W;
+var OL;
+var I18n;
+var ZIndexes = {};
